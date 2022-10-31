aeabc4d904916e990a7f62674d4487fd4f7f0198
seima <linuxseima@gmail.com>
2022-10-31 12:12:42 +0900

    トップページのHTML構成

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
-<p>Hello World</p>
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="TopPage">
+    <h1>マイ主観ランキング</h1>
+    <p>設定は簡単！あなたの好きなものをランキング付けしちゃおう！！</p>
+    <div class="ToLoginPage">
+        <button>新規登録はコチラ</br>(1分でできます!)</button>
+        <button>すでに登録済みの方</button>
+    </div>
+    <?php
+        $score = 80;
+        echo $score; 
+    ?>
+</div>
+</body>
+</html>
